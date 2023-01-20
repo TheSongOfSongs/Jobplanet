1e15c16017cfd239db83023893e3b8f33b8f04da
TheSongOfSongs <21500171@handong.edu>
2023-01-20 23:00:01 +0900

    Feat: search bar UI

--- Jobplanet/ViewController.swift
+++ Jobplanet/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupSearchBar()
     }
-
-
+    
+    // MARK: setup
+    func setupSearchBar() {
+        searchBar.delegate = self
+        
+        let image = UIImage(named: "img_logo_search")
+        searchBar.setImage(image, for: .search, state: .normal)
+        searchBar.setBackgroundImage(UIImage(), for: .any, barMetrics: .default)
+        searchBar.searchTextField.backgroundColor = .clear
+    }
 }
 
+// TODO: UISearchBarDelegate
+extension ViewController: UISearchBarDelegate {
+    
+}
