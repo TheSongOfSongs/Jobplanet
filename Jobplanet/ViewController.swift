d67d759b66edbaf95815397aaa247b6ccc856079
TheSongOfSongs <21500171@handong.edu>
2023-01-21 18:36:23 +0900

    Feat: CompanyCollectionViewCell UI

--- Jobplanet/ViewController.swift
+++ Jobplanet/ViewController.swift
@@ -38,17 +38,27 @@ class ViewController: UIViewController {
         collectionView.delegate = self
         collectionView.dataSource = self
         
-        let cell = UINib(nibName: RecruitCollectionViewCell.identifier, bundle: nil)
-        collectionView.register(cell, forCellWithReuseIdentifier: RecruitCollectionViewCell.identifier)
+        let recruitCollectionViewCell = UINib(nibName: RecruitCollectionViewCell.identifier, bundle: nil)
+        collectionView.register(recruitCollectionViewCell, forCellWithReuseIdentifier: RecruitCollectionViewCell.identifier)
+        
+        let companyCollectionViewCell = UINib(nibName: CompanyCollectionViewCell.identifier, bundle: nil)
+        collectionView.register(companyCollectionViewCell, forCellWithReuseIdentifier: CompanyCollectionViewCell.identifier)
     }
     
     func setCollectionViewSize() {
-        // TODO: 채용버튼이 선택되었을 때
+        // TODO: 채용/기업 버튼 선택되었을 때 처리
         sectionInsets = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
         minSpacing = 15
         itemsPerRow = 2
+        collectionView.collectionViewLayout = UICollectionViewFlowLayout()
         
-        // else if TODO: 기업버튼이 선택되었을 때
+//        sectionInsets = UIEdgeInsets.zero
+//        minSpacing = 0
+//        itemsPerRow = 1
+//        let companyFlowLayout = UICollectionViewFlowLayout()
+//        companyFlowLayout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+//        collectionView.collectionViewLayout = companyFlowLayout
+//        collectionView.contentInsetAdjustmentBehavior = .always
     }
 }
 
@@ -60,16 +70,18 @@ extension ViewController: UISearchBarDelegate {
 // TODO: DataSource, Delegate 채워넣기
 extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        return 5
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        // 채용 버튼 탭 되었을 경우
+        // TODO: 채용/기업 버튼 선택되었을 때 처리
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RecruitCollectionViewCell.identifier, for: indexPath) as? RecruitCollectionViewCell else {
             return UICollectionViewCell()
         }
         
-        // else if TODO: 기업버튼이 선택되었을 때
+//        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CompanyCollectionViewCell.identifier, for: indexPath) as? CompanyCollectionViewCell else {
+//            return UICollectionViewCell()
+//        }
         
         return cell
     }
@@ -81,12 +93,9 @@ extension ViewController: UICollectionViewDelegate {
 
 extension ViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
+        // 채용 버튼 탭 되었을 경우
         let paddingSpace = sectionInsets.left * 2 + minSpacing * (itemsPerRow - 1)
         let width = (view.frame.width - paddingSpace) / itemsPerRow
-        
-        // 채용 버튼 탭 되었을 경우
-        // else if TODO: 기업버튼이 선택되었을 때
         return CGSize(width: width, height: width * 226 / 160)
     }
     
