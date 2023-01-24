9e5a2e9265e575c0a8e9c4a4a1f8cf7c3b78c2f8
TheSongOfSongs <21500171@handong.edu>
2023-01-24 19:14:40 +0900

    Feat: CompanyDetailVC 추가 및 UI 작업

---
+++ Jobplanet/ViewController/CompanyDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CompanyDetailViewController.swift
+//  Jobplanet
+//
+//  Created by Jinhyang Kim on 2023/01/24.
+//
+
+import UIKit
+
+class CompanyDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
