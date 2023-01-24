746629b206fba76d34e6d2a93538f4b696870d4e
TheSongOfSongs <21500171@handong.edu>
2023-01-25 01:41:00 +0900

    Fix: ToastView가 쌓여 추가되는 문제 > shared로 변경

--- Jobplanet/Protocol/Toast.swift
+++ Jobplanet/Protocol/Toast.swift
@@ -13,44 +13,7 @@ protocol Toast where Self: UIViewController {
 
 extension Toast {
     func showAndHideToastview(with text: String) {
-        let toastView: ToastView = {
-            let leftPadding: CGFloat = 20
-            let bottomPadding = UIApplication.shared
-                .connectedScenes
-                .filter({$0.activationState == .foregroundActive})
-                .map({$0 as? UIWindowScene})
-                .compactMap({$0})
-                .first?.windows
-                .filter({$0.isKeyWindow})
-                .first?
-                .safeAreaInsets
-                .bottom
-            let screenBounds = UIScreen.main.bounds
-            let toastViewFrame = CGRect(x: leftPadding,
-                                        y: screenBounds.height - (bottomPadding ?? 0) - 100,
-                                        width: screenBounds.width - leftPadding*2,
-                                        height: 50)
-            let toastView = ToastView(frame: toastViewFrame)
-            toastView.alpha = 0
-            toastView.setText(with: text)
-            view.addSubview(toastView)
-            return toastView
-        }()
-        
-        
-        UIView.animate(withDuration: 0.2) {
-            toastView.alpha = 1
-        } completion: { success in
-            guard success else { return }
-            
-            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-                UIView.animate(withDuration: 0.2) {
-                    toastView.alpha = 0
-                } completion: { _ in
-                    toastView.removeFromSuperview()
-                }
-            }
-        }
+        ToastView.shared.showToast(with: text, view: view)
     }
 }
 
--- Jobplanet/UIView/ToastView.swift
+++ Jobplanet/UIView/ToastView.swift
@@ -28,11 +28,49 @@ class ToastView: UIView {
         commonInit()
     }
     
+    static let shared: ToastView = {
+        let leftPadding: CGFloat = 20
+        let bottomPadding = UIApplication.shared
+            .connectedScenes
+            .filter({$0.activationState == .foregroundActive})
+            .map({$0 as? UIWindowScene})
+            .compactMap({$0})
+            .first?.windows
+            .filter({$0.isKeyWindow})
+            .first?
+            .safeAreaInsets
+            .bottom
+        let screenBounds = UIScreen.main.bounds
+        let toastViewFrame = CGRect(x: leftPadding,
+                                    y: screenBounds.height - (bottomPadding ?? 0) - 100,
+                                    width: screenBounds.width - leftPadding*2,
+                                    height: 50)
+        let toastView = ToastView(frame: toastViewFrame)
+        toastView.alpha = 0
+        return toastView
+    }()
+    
     func commonInit() {
         backgroundView.makeCornerRounded(radius: 15)
     }
     
-    func setText(with text: String) {
+    func showToast(with text: String, view: UIView) {
         toastLabel.text = text
+        
+        view.addSubview(self)
+        
+        UIView.animate(withDuration: 0.2) {
+            self.alpha = 1
+        } completion: { success in
+            guard success else { return }
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                UIView.animate(withDuration: 0.2) {
+                    self.alpha = 0
+                } completion: { _ in
+                    self.removeFromSuperview()
+                }
+            }
+        }
     }
 }
