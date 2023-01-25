4778f4469e020c278e087adbc30cbe380acb2b4d
TheSongOfSongs <21500171@handong.edu>
2023-01-25 12:32:57 +0900

    Feat: MyViewModel 추가 및 데이터가져오는 메서드 구현

--- Jobplanet/ViewController/MyViewController.swift
+++ Jobplanet/ViewController/MyViewController.swift
@@ -6,10 +6,14 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class MyViewController: UIViewController {
     
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    let viewModel = MyViewModel()
     
     var recruits: [RecruitItem] = []
     
@@ -17,13 +21,28 @@ class MyViewController: UIViewController {
     let minSpacing: CGFloat = 15
     let itemsPerRow: CGFloat = 2
     
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupCollectionView()
+        setupBinding()
     }
     
     
     // MARK: - setup
+    func setupBinding() {
+        let input = MyViewModel.Input()
+        let output = viewModel.transform(input: input)
+        
+        output.recruitItems
+            .drive(with: self, onNext: { owner, recruitItems in
+                owner.recruits = recruitItems
+                owner.collectionView.reloadData()
+            })
+            .disposed(by: disposeBag)
+    }
+    
     func setupCollectionView() {
         let recruitCollectionViewCell = UINib(nibName: RecruitCollectionViewCell.identifier, bundle: nil)
         collectionView.register(recruitCollectionViewCell, forCellWithReuseIdentifier: RecruitCollectionViewCell.identifier)
---
+++ Jobplanet/ViewModel/MyViewModel.swift
@@ -0,0 +1,84 @@
+//
+//  MyViewModel.swift
+//  Jobplanet
+//
+//  Created by Jinhyang Kim on 2023/01/25.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class MyViewModel: ViewModel {
+    struct Input { }
+    
+    struct Output {
+        let recruitItems: Driver<[RecruitItem]>
+        let error: Driver<APIServiceError>
+    }
+    
+    private let bookedRecruitItemsRelay = PublishRelay<[RecruitItem]>()
+    private let errorRelay = PublishRelay<APIServiceError>()
+    
+    let disposeBag = DisposeBag()
+    
+    private let networkService = NetworkService()
+    
+    // MARK: -
+    init() { }
+    
+    // MARK: -
+    func transform(input: Input) -> Output {
+        Task { [weak self] in
+            guard let self = self else { return }
+            
+            do {
+                let ids = self.fetchBookMarkedRecruitIds()
+                let result = try await self.fetchRecruitItems()
+                
+                switch result {
+                case .success(let items):
+                    let items =  self.filterRequestItems(items, with: ids)
+                    self.bookedRecruitItemsRelay.accept(items)
+                case .failure(let error):
+                    self.errorRelay.accept(error)
+                }
+                
+            } catch let error {
+                self.errorRelay.accept(error as? APIServiceError ?? .unknown)
+            }
+        }
+        
+        let recruitItems = bookedRecruitItemsRelay.asDriver(onErrorJustReturn: [])
+        let error = errorRelay.asDriver(onErrorJustReturn: APIServiceError.unknown)
+        return Output(recruitItems: recruitItems,
+                      error: error)
+    }
+    
+    private func fetchBookMarkedRecruitIds() -> [Int] {
+        let ids = UserDefaultsHelper.getData(type: [Int].self, forKey: .recruitIdsBookMarkOn) ?? []
+        return ids
+    }
+    
+    private func fetchRecruitItems() async throws -> Result<[RecruitItem], APIServiceError> {
+        do {
+            let results = try await networkService.recruitItems()
+            switch results {
+            case .success(let items):
+                return .success(items)
+            case .failure(let error):
+                NSLog("❗️ 에러 - ", error.localizedDescription)
+                return .failure(.unknown)
+            }
+        } catch let error {
+            NSLog("❗️ 에러 - ", error.localizedDescription)
+            return .failure(.unknown)
+        }
+    }
+    
+    private func filterRequestItems(_ recruitItems: [RecruitItem], with ids: [Int]) -> [RecruitItem] {
+        let result = recruitItems.filter({ ids.contains($0.id) })
+        return result
+    }
+}
+
